ac09a269d88cd3a90b32cf3bdfb9cf82e4ef4f50
PeterGamez <chanakan.kea@gmail.com>
2026-02-19 12:54:35 +0700

    Refactor Password utility methods to ensure consistent use of await for asynchronous operations

--- backend/src/utils/Password.ts
+++ backend/src/utils/Password.ts
@@ -4,10 +4,10 @@ const SALT_ROUNDS = 10;
 
 export class Password {
     public async hash(password: string): Promise<string> {
-        return bcrypt.hash(password, SALT_ROUNDS);
+        return await bcrypt.hash(password, SALT_ROUNDS);
     }
 
     public async compare(password: string, hash: string): Promise<boolean> {
-        return bcrypt.compare(password, hash);
+        return await bcrypt.compare(password, hash);
     }
 }

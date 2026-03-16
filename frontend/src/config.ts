a08024d8a5bb56542c614fa6926bdf27d3b1dabd
PeterGamez <chanakan.kea@gmail.com>
2026-03-16 14:00:53 +0700

    feat: enhance configuration and metadata for improved SEO and site settings

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
 const config = {
+  siteUrl: process.env.SITE_URL || "http://localhost:3000",
   apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
   socketUrl: process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3002",
 };
